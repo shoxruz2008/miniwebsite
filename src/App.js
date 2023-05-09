68b05f89f61eb6a8beec3e8bfc3d438878e2d9e3
Shoxruz <shoxruzarimona@gmail.com>
2023-05-09 22:53:41 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Navbar from './componets/miniwebsite/Navbar';
+import Hero from './componets/miniwebsite/Hero';
+import Card from './componets/miniwebsite/Card';
+
+import { data } from './data';
+
+const App = () => {
+	const cards = data.map((item) => {
+		return (
+			<Card
+				key={item.id}
+				img={item.coverImg}
+				rating={item.stats.rating}
+				reviewCount={item.stats.reviewCount}
+				location={item.location}
+				title={item.title}
+				price={item.price}
+			/>
+		);
+	});
+	return (
+		<div className="container">
+			<Navbar />
+			<Hero />
+
+			<section className="cards-list">{cards}</section>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/componets/miniwebsite/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+	<nav>
+		<img className='nav--logo' src='../../../images/Logo.png' alt="Logo" />
+	</nav>
+  )
+}
+
+export default Navbar
